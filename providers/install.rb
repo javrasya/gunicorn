0b4b1f846cdb31898f85771230de510c0e201613
Ahmet DAL <ceahmetdal@gmail.com>
2014-12-25 03:48:28 +0200

    minor fix

--- providers/install.rb
+++ providers/install.rb
@@ -20,6 +20,8 @@
 
 action :install do
   python_virtualenv new_resource.virtualenv do
+	owner new_resource.owner  	
+	groupt owner new_resource.group
     action :create
   end if new_resource.virtualenv
 
